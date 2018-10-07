8b770054f215d351338264dfd2f6a4edc65b0942
nastprol <nastprol@users.noreply.github.com>
2018-10-07 19:25:13 +0500

    Исправлено сравнение

--- src/main/scala/fintech/homework02/ComplexNumber.scala
+++ src/main/scala/fintech/homework02/ComplexNumber.scala
@@ -10,24 +10,40 @@ class ComplexNumber(val real: Double, val image: Double) {
     new ComplexNumber(real * b.real - image * b.image, image * b.real + real * b.image)
   }
 
-  private def GetModule(): Double = math.sqrt(real * real + image * image)
+  private def GetModulus(): Double = math.sqrt(real * real + image * image)
 
   private def GetArgument(): Double = math.atan(image / real)
 
   def ~(pow: Int): ComplexNumber = {
-    val module = GetModule()
+    val module = GetModulus()
     val arg = GetArgument()
     val moduleInPow = math.pow(module, pow)
     new ComplexNumber(math.cos(pow * arg) * moduleInPow, math.sin(pow * arg) * moduleInPow)
   }
 
-  override def toString: String = {
-    if (image > 0) real.toString + " + " + image.toString + "i"
-    else if (image < 0) real.toString + " - " + math.abs(image).toString + "i"
-    else real.toString
+  private def imageToString():String=math.abs(image) match {
+    case 1 => "i"
+    case 0 => ""
+    case _ => math.abs(image).toString + "i"
   }
 
-  override def equals(a: Any): Boolean = a.isInstanceOf[ComplexNumber] && a.asInstanceOf[ComplexNumber] == this
+  override def toString: String = {
+    if (real == 0 && image == 0)
+      return "0"
+    real match {
+      case 0 => (if (image < 0) "-") + imageToString()
+      case _ =>
+        val sign = if (image < 0) " - " else if (image > 0) " + " else ""
+        real.toString + sign + imageToString()
+    }
+  }
+
+  override def equals(a: Any): Boolean = {
+    a match {
+      case complex: ComplexNumber => real == complex.real && image == complex.image
+      case _ => false
+    }
+  }
 
   override def hashCode(): Int = {
     val prime = 97
@@ -36,8 +52,6 @@ class ComplexNumber(val real: Double, val image: Double) {
     hash = prime * hash + real.hashCode()
     hash
   }
-
-  def ==(b: ComplexNumber): Boolean = real == b.real && image == b.image
 
   // Написать класс описывающий комплексные числа.
   // Реализовать проверку на равенство, умножение и сложение, toString.
--- src/test/scala/fintech/homework02/ComplexNumberSpec.scala
+++ src/test/scala/fintech/homework02/ComplexNumberSpec.scala
@@ -7,37 +7,61 @@ class ComplexNumberSpec extends FlatSpec with Matchers {
     val a = new ComplexNumber(3, 8)
     val b = new ComplexNumber(2, 1)
     val c = a + b
+    val d = new ComplexNumber(1, 1)
+
     c should be (new ComplexNumber(5, 9))
+    c should be (b + a)
+    a + new ComplexNumber(0,0) should be (a)
+    (a + b) + d should be (a + (b + d))
   }
 
   "Composition of (a+bi)*(c+di)" should "be equal to (ac-bd)+(bc-ad)i" in {
     val a = new ComplexNumber(3, 4)
     val b = new ComplexNumber(2, 3)
     val c = a * b
+    val d = new ComplexNumber(2, 2)
+
     c should be (new ComplexNumber(-6, 17))
+    c should be (b * a)
+    (a * b) * d should be (a * (b * d))
+    a * new ComplexNumber(1,0) should be (a)
+    a * new ComplexNumber(0,0) should be (new ComplexNumber(0, 0))
+    a * (b + d) should be (a * b + a * d)
+    (a + b) * d should be (a * d + b * d)
   }
 
   "ComplexNumber (a+bi) and (c+di)" should "be equal if a == c and b == i" in {
     val a = new ComplexNumber(3, 2)
     val b = new ComplexNumber(2, 3)
     val c = new ComplexNumber(2, 3)
+
     a == b should be (false)
+    b == a should be (false)
     b == c should be (true)
+    c == b should be (true)
+    a == null should be (false)
+    a == a should be (true)
   }
 
   "String of complex number (a+bi)" should "be a + bi" in {
     val a = new ComplexNumber(3, -2)
     val b = new ComplexNumber(2, 3)
-    val c = new ComplexNumber(2, 0)
+    val c = new ComplexNumber(-2, 0)
+    val d = new ComplexNumber(0, -2)
+    val e = new ComplexNumber(0, 0)
+
     a.toString() should be ("3.0 - 2.0i")
     b.toString() should be ("2.0 + 3.0i")
-    c.toString() should be ("2.0")
+    c.toString() should be ("-2.0")
+    d.toString() should be ("-2.0i")
+    e.toString() should be ("0")
   }
 
   "Complex number z in pow n" should "|z|^n*(cos(nf)+sin(nf)*i)" in {
     val a = new ComplexNumber(5, 2)
     val c = a ~ 2
     val eps = 0.00000001
+
     math.abs(c.real - 21) < eps should be (true)
     math.abs(c.image - 20) < eps should be (true)
   }
